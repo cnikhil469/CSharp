79fc7dd9efcc56a0bf09e3b854c9af0ff8dabc86
Nikhil Chakravarthy <59420126+cnikhil469@users.noreply.github.com>
2024-08-11 19:24:24 -0500

    initial commit

---
+++ projectCsharp/projectCsharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine(); 
+            int x = 10,
+                y = 20,
+                z = 30;
+            int f;
+
+            double val = 25.000D;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+            Console.ReadLine();
+        }
+    }
+}
