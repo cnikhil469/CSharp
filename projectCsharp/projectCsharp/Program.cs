d13385832204283df46b8c2b8feadf08cab935d3
nikhil980 <cnikhil2010@gmail.com>
2024-08-16 15:40:40 -0500

    included for loop changes and updated the gitignore file

--- projectCsharp/projectCsharp/Program.cs
+++ projectCsharp/projectCsharp/Program.cs
@@ -33,6 +33,99 @@ namespace projectCsharp
             Console.WriteLine(decimal.MaxValue);
             Console.WriteLine(decimal.MinValue);
 
+            //var b = 12345;
+            //usage of const variable
+            const int k = 50;
+            //k = 90;
+            Console.WriteLine(k);
+            //calulating the 2 sets of remainders task
+            int a = 100;
+            int b = 20;
+            double c = b % a;
+            Console.WriteLine(c);
+            b = 25;
+            //c = a % b;
+            Console.WriteLine(c);
+
+            //readline practice
+            Console.WriteLine("enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("you entered this: ");
+            Console.WriteLine(name);
+
+
+            //string concatination
+            string age = Console.ReadLine();
+            Console.WriteLine("My name is " + name + " and my age is " + age);
+            //conversion of age
+            int ageInt = Convert.ToInt32(age);
+            Console.WriteLine(ageInt);
+            int age2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age2);
+
+            //justchecking one impl of if statement.
+            var a1 = Convert.ToInt32(Console.ReadLine());
+            var b1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is the value of " + a1 + "*" + b1 + ": ");
+            var c2 = Convert.ToInt32(Console.ReadLine());
+            var c1 = a1 * b1;
+            if (c1 == c2)
+            {
+                Console.WriteLine("Yahoo!!");
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+            //Console.WriteLine(c1);
+
+            //switch case statement
+            Console.Write("enter a number such that 1 <= number <= 8");
+            int dayNum = Convert.ToInt32(Console.ReadLine());
+
+            switch(dayNum)
+            {
+                case 1:
+                    Console.WriteLine("mon");
+                    break;
+                case 2:
+                    Console.WriteLine("tue");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Wed");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 8:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("give a 1 <= number <= 8");
+                    break;
+            }
+
+            Console.Write("repetition word? ");
+            string word = Console.ReadLine();
+            Console.Write("how many times? ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 0)
+            {
+                Console.WriteLine("number > 0");
+            }
+            else {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine(word);
+                }
+            }
             Console.ReadLine();
         }
     }
